b12bf264ca7d20c45160e809a7e4def406c7e1a1
caojing <641809386@qq.com>
2021-03-02 17:01:18 +0800

    授权许可证申明

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -47,10 +47,10 @@ import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
 
-    var names = arrayOf("安琪拉baby", "小熊熊", "俊介", "小吉吉", "老爷爷", "巴格达", "小妲己", "柯南")
-    var alias = arrayOf("泰迪", "比熊", "博美", "苹果头", "小老头，倔老头", "哈巴狗", "迷你杜宾", "名侦探")
-    var breads = arrayOf("贵宾犬", "比熊犬", "博美犬", "吉娃娃", "雪纳瑞", "巴哥犬", "鹿犬", "柯基")
-    var introduces = arrayOf(
+    private var names = arrayOf("安琪拉baby", "小熊熊", "俊介", "小吉吉", "老爷爷", "巴格达", "小妲己", "柯南")
+    private var alias = arrayOf("泰迪", "比熊", "博美", "苹果头", "小老头，倔老头", "哈巴狗", "迷你杜宾", "名侦探")
+    private var breads = arrayOf("贵宾犬", "比熊犬", "博美犬", "吉娃娃", "雪纳瑞", "巴哥犬", "鹿犬", "柯基")
+    private var introduces = arrayOf(
         "最常见的是红色（也叫棕色）中小体型的贵宾犬 除此之外还有黑色、灰色、白色等颜色的贵宾犬 其体型也有茶杯体贵宾至巨型贵宾之分 此类犬是小型犬中智商最高者 性格活泼 且最大的优点就是不会像其他汪星人一样大量掉毛 但也正因为如此 它需要定期理毛修毛",
         "此类犬通常情况下只有白色一种颜色 常与白色贵宾犬混淆 两者比较容易分辨的一个区别是贵宾犬眼睛是类似于杏仁形状的 而比熊的眼睛是圆形的 它与贵宾犬的属性相似 同样比较聪明 不爱掉毛 需要经常理毛修毛 但性格方面两者相较 比熊会比较憨厚一点 贵宾的性格则偏向于活泼",
         "最出名的博美犬是一只名叫“俊介”的黄色博美 因其可爱的造型被众多网友称赞 现“俊介”除了是一只汪的名字 还是博美这一类汪星人的一种美容造型 除了黄色 常见的博美颜色还有白色 这种犬的毛也是需要经常打理 换毛期的时候 会有掉毛现象 性格方面 比较像一个被宠爱的小公主",
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         "有人称之为“迷你杜宾”常见颜色为黑红色（有人称之为“铁包金”）和红色 四肢修长 体型匀称 被毛短 随着年龄增长耳朵会渐渐立直 性格机警活泼",
         "英国女王最爱的犬 小短腿 性格友好温顺 聪明善解人意 充满活力（个人觉得这种犬 小短腿的基因特别强大）"
     )
-    var imageIds = arrayOf(
+    private var imageIds = arrayOf(
         R.drawable.guibin,
         R.drawable.bixion,
         R.drawable.bomei,
